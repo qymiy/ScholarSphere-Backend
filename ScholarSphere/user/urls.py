3d5683dbd6b5a0895798e0408d0814763c58da2a
WenjiaRan <399010199@qq.com>
2023-04-16 20:10:59 +0800

    build django structure

---
+++ ScholarSphere/user/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path('autologin',autologin),
+    # path('')
+    # 这是一个样例，指定路由名为url_name，对应处理函数为当前app内views.py中的api_name
+]
