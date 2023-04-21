3d0399c793edb263627e82f7f73f40f32b3d22cc
qymiy <2718106017@qq.com>
2023-04-21 22:34:44 +0800

    jmy第一次提交

--- ScholarSphere/user/urls.py
+++ ScholarSphere/user/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('user/register', register),
-    path('user/checkmailregistered',checkmailregistered )
+    path('user/checkmailregistered',checkmailregistered),
+    path('user/autologin',autologin),
+    path('user/login',login)
     # 这是一个样例，指定路由名为url_name，对应处理函数为当前app内views.py中的api_name
 ]
