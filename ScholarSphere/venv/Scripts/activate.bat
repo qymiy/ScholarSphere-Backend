09808cda0ac1e1a31458f801cd17cc5b708b0201
WenjiaRan <399010199@qq.com>
2023-04-25 20:33:51 +0800

    ss

--- ScholarSphere/venv/Scripts/activate.bat
+++ ScholarSphere/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Jianmy09\Desktop\ScholarSphere-Backend\ScholarSphere\venv
+set VIRTUAL_ENV=D:\MyFiles\4thSemester\SoftEngineer\ScholarSphere-Backend\ScholarSphere\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
